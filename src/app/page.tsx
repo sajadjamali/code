8b11ad3903451500193a1cf8b0a8671c42e649b5
sajadjamali <125208168+sajadjamali@users.noreply.github.com>
2024-05-02 16:31:08 +0330

    test line

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 
 const Page = async () => {
 
-  const res = await fetch('http://localhost:3000//api', {cache: 'no-store'})
+  const res = await fetch('http://localhost:3000//api', { cache: 'no-store' })
   const response = await res.json();
 
   return (
-    <div>{response.message}</div>
+    <>
+    <p>Test</p>
+      <div>{response.message}</div>
+    </>
   )
 };
 
