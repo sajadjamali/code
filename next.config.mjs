76e119a33df40657fccbebdc9212018f2407213f
sajadjamali <125208168+sajadjamali@users.noreply.github.com>
2024-05-14 15:04:14 +0330

    sssssssss

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // images: {
-    //     remotePatterns: [
-    //         {
-    //             protocol: 'https',
-    //             hostname: 'image.tmdb.org',
-    //         },
-    //     ],
-    // },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'image.tmdb.org',
+            },
+            {
+                protocol: 'http', // Use http for localhost (HTTPS might not be set up)
+                hostname: 'localhost',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,39 +15,50 @@ const fetcherFunc = async (url: string) => {
   }
 }
 
-async function getMovies(categorie: string, page: number) {
-  const movies = await fetcherFunc(`${baseUrl}/movie/${categorie}?page=${page}&api_key=${api_key}`)
-  return movies
+export async function getMovie(id: string) {
+  const movie = await fetcherFunc(`${baseUrl}/movie/${id}?append_to_response=videos,credits&api_key=${api_key}`);
+  return movie
 }
 
 const Page = async () => {
 
-  const movies = await getMovies('top_rated', 3);
+  const movie = await getMovie('157336');
+
+  // console.log(movie)
 
   return (
+    
+    // <div className='a b c'><p className='ds'>guououououoisd</p></div>
+    <Image src='https://image.tmdb.org/t/p/w500/xJHokMbljvjADYdit5fK5VQsXEG.jpg' width={200} height={200} alt='not found' />
+    // <Image src='https://www.pexels.com/photo/blue-and-purple-cosmic-sky-956999/' width={200} height={200} alt='not found' />
 
-    <section className="mt-6 grid min-[400px]:grid-cols-2 min-[650px]:grid-cols-3 min-[1050px]:grid-cols-4 min-[1450px]:grid-cols-5 min-[1700px]:grid-cols-6">
-      {
-        movies.results.map((movie: any, index: number) => (
-          <div key={index}>
-            <div className='relative w-72 h-96 overflow-hidden'>
-              <Image
-                fill
-                className='w-auto'
-                alt={movie.title}
-                // width={250}
-                // height={250}
-                src={`${imgBaseUrl}${movie.poster_path}`}
-                unoptimized
-              />
-              {/* <p key={index}>{movie.original_title}</p> */}
-            </div>
-            <p key={index}>{movie.original_title}</p>
-          </div>
-        ))
-      }
-      {/* <Image src='/img.png' width={200} height={200} alt='not found' /> */}
-    </section>
+    // <>
+    //   <div style={{ width: '200px', height: '100px' }} className='bg-red-700'>salam</div>
+    //   <div>
+    //     <div style={{ width: '200px', height: '100px', position: 'relative', bottom: '40px' }} className='bg-blue-700'>salam</div>
+    //   </div>
+    // </>
+
+    // <section className="mt-6 gap-10 grid min-[400px]:grid-cols-2 min-[650px]:grid-cols-3 min-[1050px]:grid-cols-4 min-[1450px]:grid-cols-5 min-[1700px]:grid-cols-6">
+    //   {
+    //     movies.results.map((movie: any, index: number) => (
+    //       <div key={index}>
+    //         <div className='relative w-64 h-96 overflow-hidden'>
+    //           <Image
+    //             fill
+    //             className='w-auto'
+    //             alt={movie.title}
+    //             // width={250}
+    //             // height={250}
+    //             src={`${imgBaseUrl}${movie.poster_path}`}
+    //             unoptimized
+    //           />
+    //         </div>
+    //         <p key={index}>{movie.original_title}</p>
+    //       </div>
+    //     ))
+    //   }
+    // </section>
   )
 
 };
